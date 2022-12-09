9c14bf8b37496094bed6346be45c46f64e23b72f
BreckonJ <92556555+BreckonJ@users.noreply.github.com>
2022-12-08 20:52:02 -0400

    Speed display by empty query

--- db/QuizAccessor.php
+++ db/QuizAccessor.php
@@ -25,6 +25,40 @@ class QuizAccessor {
             $quizTitle = $dbquiz["quizTitle"];
             $questions = $questionAcc->getQuestionsForQuiz($quizID);
             $points = $this->getPointsForQuiz($quizID);
+            $questions = null;
+            $points = null;
+            $result = new Quiz($quizID, $quizTitle, $questions, $points);
+        } catch (Exception $e) {
+            $result = null;
+        } finally {
+            if (!is_null($stmt)) {
+                $stmt->closeCursor();
+            }
+        }
+
+        return $result;
+    }
+    
+    public function getQuizByIDEmpty($quizID) {
+        $result = null;
+        $stmt = null;
+        try {
+            $conn = connect_db();
+            $stmt = $conn->prepare("select * from Quiz where quizID = :quizID");
+            $stmt->bindParam(":quizID", $quizID);
+            $stmt->execute();
+            $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (count($dbresults) !== 1) {
+                throw new Exception("duplicate quizIDs found in Quiz table!");
+            }
+
+            $dbquiz = $dbresults[0];
+            $questionAcc = new QuestionAccessor();
+            $quizTitle = $dbquiz["quizTitle"];
+            //$questions = $questionAcc->getQuestionsForQuiz($quizID);
+            //$points = $this->getPointsForQuiz($quizID);
+            $questions = null;
+            $points = null;
             $result = new Quiz($quizID, $quizTitle, $questions, $points);
         } catch (Exception $e) {
             $result = null;
--- db/QuizResultAccessor.php
+++ db/QuizResultAccessor.php
@@ -40,6 +40,41 @@ class QuizResultAccessor {
 
         return $results;
     }
+    
+    public function getResultsByQueryEmpty($query) {
+        $results = [];
+        $stmt = null;
+        $dbresults = null;
+        try {
+            $conn = connect_db();
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            $results = [];
+        } finally {
+            if (!is_null($stmt)) {
+                $stmt->closeCursor();
+            }
+        }
+
+        $quizAcc = new QuizAccessor();
+
+        foreach ($dbresults as $r) {
+            $quiz = $quizAcc->getQuizByIDEmpty($r["quizID"]);
+            $resultID = $r["resultID"];
+            $username = $r["username"];
+            $quizStartTime = $r["quizStartTime"];
+            $quizEndTime = $r["quizEndTime"];
+            $userAnswers = $r["userAnswers"];
+            $scoreNumerator = $r["scoreNumerator"];
+            $scoreDenominator = $r["scoreDenominator"];
+            $obj = new QuizResult($resultID, $quiz, $username, $quizStartTime, $quizEndTime, $userAnswers, $scoreNumerator, $scoreDenominator);
+            array_push($results, $obj);
+        }
+
+        return $results;
+    }
 
     public function getAllResults() {
         return $this->getResultsByQuery("select * from QuizResult");
@@ -50,6 +85,6 @@ class QuizResultAccessor {
     }
 
     public function getResultsByScore($scoremin, $scoremax) {        
-        return $this->getResultsByQuery("select * from quizresult where (100*scoreNumerator/scoreDenominator) between " . $scoremin . " AND " . $scoremax);
+        return $this->getResultsByQueryEmpty("select * from quizresult where (100*scoreNumerator/scoreDenominator) between " . $scoremin . " AND " . $scoremax);
     }
 }
