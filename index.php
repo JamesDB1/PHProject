35bb4a71670a05e28f5efe674e91b8bb23257a3b
BreckonJ <92556555+BreckonJ@users.noreply.github.com>
2022-12-16 19:47:22 -0400

    Menu and menu styles

--- index.php
+++ index.php
@@ -6,23 +6,32 @@
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <div class="container">
+        <div class="menuContainer">
             <h1>PHP Project - QuizApp Deluxe</h1>
             <h3>Welcome, Guest!</h3>
             <div class="tempMenu">
-                <h2>Temporary Menu</h2>
-                <a href ="takeQuiz.php">Take a quiz</a> 
-                <a href="quizSearch.php">Search quizzes and questions</a>
-                <a href="quizzes.php">Search quiz results</a>
+
+                <div class="menuContainer">
+                    <a class="active" href="takeQuiz.php">Home</a>
+                    <a href="quizSearch.php">News</a>
+                    <a href="quizzes.php">Contact</a>
+                    <a href="question.php">Search questions</a>
+                    <div class="menuContainer-right">
+                        <a id="loginButton" href="login-js.php">Log in</a>
+                        <a id="createAccountButton" href="newUser-guest.php">Sign up</a>
+                    </div>
+                </div>
+
+
+
             </div>
             <div class='loginMenu'>
                 <h2>Login Menu</h2>
                 <div>
-                    <a id="loginButton" href="login-js.php">Log in</a>
-                    <a id="createAccountButton" href="newUser-guest.php">Sign up</a>
+
                 </div>
             </div>
-            
+
         </div>
     </body>
 </html>
