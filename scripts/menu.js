009bfd6df601d69c5f84a07f5d6d58ff6ac844b7
jimlondon-nbcc <90627675+jimlondon-nbcc@users.noreply.github.com>
2022-12-16 20:09:49 -0400

    Added modify users to menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,7 @@ window.onload = function () {
     //Add menu to menuContainer
     let menuhtml = '<a class="active" href="takeQuiz.php">Home</a><a href="quizSearch.php">News</a>';
     menuhtml += '<a href="quizzes.php">Contact</a><a href="question.php">Search questions</a>';
+    menuhtml += `<a href='modifyUser.php'>Modify Users</a>`;
     menuhtml += '<div class="menuContainer-right"><a id="loginButton" href="login-js.php">Log in</a>';
     menuhtml += '<a id="createAccountButton" href="newUser-guest.php">Sign up</a></div>';
     document.querySelector(".menuContainer").innerHTML = menuhtml;
--- scripts/quizResultDetails.js
+++ scripts/quizResultDetails.js
@@ -1,5 +1,27 @@
 let quizResult;
-window.onload = function(){
+window.onload = function () {
     const resultID = document.querySelector("#qrID").innerHTML.trim();
     console.log("Processing request for " + resultID);
+
+    fetchResult(resultID);
 };
+
+async function fetchResult(id) {
+    const url = `quizapp/`
+
+
+}
+
+function buildResultTable(quizResult) {
+    let tableLocation = document.querySelector("#displayQuizRecord");
+    //open resultsArea div and table
+    let tableHTML = "<div id = 'quizResultsArea'><table>";
+    //build table header row
+    tableHTML += "<tr>";
+    tableHTML += "<th>Question</th>";
+    tableHTML += "<th>Question Text</th>";
+    tableHTML += "<th>Correct Answer</th>";
+    tableHTML += "<th>Your Answer</th>";
+    tableHTML += "<th>Score</th>";
+    tableHTML += "</tr>";
+}
