7d03cc7203352a57683df363068bca003142fee0
BreckonJ <92556555+BreckonJ@users.noreply.github.com>
2022-12-16 20:00:22 -0400

    Menu in js for managing

--- index.php
+++ index.php
@@ -4,34 +4,10 @@
         <meta charset="UTF-8">
         <title>Home Page</title>
         <link rel="stylesheet" href="styles.css">
+        <script src="scripts/menu.js"></script>
     </head>
     <body>
         <div class="menuContainer">
-            <h1>PHP Project - QuizApp Deluxe</h1>
-            <h3>Welcome, Guest!</h3>
-            <div class="tempMenu">
-
-                <div class="menuContainer">
-                    <a class="active" href="takeQuiz.php">Home</a>
-                    <a href="quizSearch.php">News</a>
-                    <a href="quizzes.php">Contact</a>
-                    <a href="question.php">Search questions</a>
-                    <div class="menuContainer-right">
-                        <a id="loginButton" href="login-js.php">Log in</a>
-                        <a id="createAccountButton" href="newUser-guest.php">Sign up</a>
-                    </div>
-                </div>
-
-
-
-            </div>
-            <div class='loginMenu'>
-                <h2>Login Menu</h2>
-                <div>
-
-                </div>
-            </div>
-
         </div>
     </body>
 </html>
